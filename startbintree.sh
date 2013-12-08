cd88f28daac6b0680f2a4a2a7453223acf5350cb
Ubuntu <ubuntu@ip-172-31-16-81.us-west-2.compute.internal>
2013-12-08 20:14:33 +0000

    Fix scripts

--- startbintree.sh
+++ startbintree.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./pox/pox.py log.level --DEBUG misc.bintreeswitch &
+./pox/pox.py misc.bintreeswitch > /dev/null &
 sudo mn --topo tree,3 --mac --arp --switch ovsk --controller=remote
--- startfattree.sh
+++ startfattree.sh
@@ -1,3 +1,3 @@
-!#/bin/sh
-./pox/pox.py log.level --DEBUG misc.fattreeswitch &
-sudo mn --custom /mininet/custom/topo-fattree.py --topo fattree --mac --arp --switch ovsk --controller=remote
+#!/bin/sh
+./pox/pox.py misc.fattreeswitch > /dev/null &
+sudo mn --custom ~/mininet/custom/topo-fattree.py --topo fattree --mac --arp --switch ovsk --controller=remote
