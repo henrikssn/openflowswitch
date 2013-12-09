1f81b5ab7b0f0c75574b0b0df5da5c69aa38428b
Ubuntu <ubuntu@ip-172-31-16-81.us-west-2.compute.internal>
2013-12-09 09:43:30 +0000

    Build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+!#/bin/sh
+echo "Henriksson & Burkhalter build script v 1.0.0"
+cp fattreeswitch.py pox/pox/misc/
+cp bintreeswitch.py pox/pox/misc/
+cp hostlist.csv pox/
+cp hostlist_fattree.csv pox/
+echo "Build done!"
