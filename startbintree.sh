57c9267466f408648893b98e7e53bb72dc85736c
Ubuntu <ubuntu@ip-172-31-16-81.us-west-2.compute.internal>
2013-12-09 09:33:59 +0000

    Kill controller on exit

--- startbintree.sh
+++ startbintree.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-./pox/pox.py misc.bintreeswitch > /dev/null &
+./pox/pox.py misc.bintreeswitch &
 sudo mn --topo tree,3 --mac --arp --switch ovsk --controller=remote
+sudo killall python2.7
--- startfattree.sh
+++ startfattree.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-./pox/pox.py misc.fattreeswitch > /dev/null &
+./pox/pox.py misc.fattreeswitch &
 sudo mn --custom ~/mininet/custom/topo-fattree.py --topo fattree --mac --arp --switch ovsk --controller=remote
+sudo killall python2.7
